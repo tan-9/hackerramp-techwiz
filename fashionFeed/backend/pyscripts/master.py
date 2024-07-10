368c00d33b97b2f09ce3adb69c0e6b946a06d45b
Tanaya <tanayamohanty0909@gmail.com>
2024-07-11 00:56:21 +0530

    working on recommendation system

--- fashionFeed/backend/pyscripts/master.py
+++ fashionFeed/backend/pyscripts/master.py
@@ -1,13 +1,38 @@
 import pandas as pd
 
 # Load the master products database
-df = pd.read_json('https://query.data.world/s/tf7lztreshn5ebbh3zlkhvslpeqfv7?dws=00000')
+master_df = pd.read_csv('fashionFeed/backend/files/master.csv')
 
-print(df.head())
+# print(master_df.head())
 
-df.drop(df.columns[[1,2,5]], axis=1, inplace=True)
-print(df.columns)
+master_df.drop(master_df.columns[3], axis=1, inplace=True)
 
-df.rename(columns={'name':'product'}, inplace=True)
-print(df.dtypes)
+master_df.rename(columns={'seller':'brand'}, inplace=True)
+print(master_df.columns)
+
+userOrders_df = pd.read_csv('fashionFeed/backend/files/userOrders.csv')
+userWishlist_df = pd.read_csv('fashionFeed/backend/files/userWishlist.csv')
+userSearch_df = pd.read_csv('fashionFeed/backend/files/userSearch.csv')
+
+userOrders_df.fillna(0, inplace=True)
+userWishlist_df.fillna(0, inplace=True)
+# print(userOrders_df.isnull().sum())
+# print(userWishlist_df.isnull().sum())
+# print(userSearch_df.isnull().sum())
+
+print(master_df['purl'].iloc[0])
+
+def get_category(url):
+    url_split = url.split('/')
+    category = url_split[3]
+    return category
+
+master_df['category'] = master_df['purl'].apply(lambda x: pd.Series(get_category(x)))
+
+print(master_df.head())
+
+
+
+
+
 
---
+++ fashionFeed/backend/pyscripts/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+master_df.rename(columns={'name':'product'}, inplace=True)
