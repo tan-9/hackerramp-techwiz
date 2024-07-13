077711b28ad8127194d437afe255ab9175d37ee0
Tanaya <tanayamohanty0909@gmail.com>
2024-07-13 10:24:48 +0530

    recommendation system changes

---
+++ fashionFeed/backend/integration/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+@app.route('/recommendations', methods = ['GET'])
+
+def get_recommendations():
+    project_dir = "C:/Users/tanay/Desktop/tan/myntra-hackerramp/fashionFeed"
+    data_dir = os.path.join(project_dir, "data")
+    file_path = os.path.join(data_dir, "recommendations.csv")
+
+    recommendations_df = pd.read_csv(file_path)
+    recommendations = recommendations_df.to_dict(orient='records')
+    return jsonify(recommendations)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
